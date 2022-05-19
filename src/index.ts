35984179df01428efff856b0c218cef337891768
João Pedro <estudante2025@gmail.com>
2022-05-19 00:10:39 -0300

    Finalizado!

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import express, { Request, Response, NextFunction} from "express";
+import express from "express";
+import statusRoute from "./routes/status.route";
+import usersRoute from './routes/users.route';
+
+
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Teste6'});
-});
+//configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+//configuração das rotas
+app.use(usersRoute);
+app.use(statusRoute)
 
 
+//inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação rodando na porta 3000')
 })
